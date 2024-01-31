873be57943ef85d9d7119ec4ea6ce515ee421127
Ryan McBride <mcbrider@shernet.sheridancollege.ca>
2024-01-31 10:58:47 -0500

    make it move and follow path

--- Assets/Week 4/Scripy/Plane.cs
+++ Assets/Week 4/Scripy/Plane.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Plane : MonoBehaviour
@@ -9,21 +10,68 @@ public class Plane : MonoBehaviour
     {
         trailPath = GetComponent<LineRenderer>();
         trailPath.positionCount = 1;
-
         trailPath.SetPosition(0, transform.position);
+
+        rigidbody = GetComponent<Rigidbody2D>();
     }
+
+    //this updates every time our rigidbody moves
+    private void FixedUpdate()
+    {
+        currentPosition = new Vector2(transform.position.x, transform.position.y);
+        if(points.Count > 0)
+        {
+            Vector2 direction = points[0] - currentPosition;
+            float angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+            rigidbody.rotation = -angle;
+            
+        }
+        rigidbody.MovePosition(rigidbody.position + (Vector2)transform.up*speed*Time.deltaTime);
+    }
+
+
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            landingTime += 0.1f * Time.deltaTime;
+            float interpolation = Landing.Evaluate(landingTime);
+            if(transform.localScale.x < 0.1f)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                transform.localScale = Vector3.Lerp(transform.localScale, Vector3.zero, interpolation);
+            }
+        }
 
+        if(points.Count > 0)
+        {
+            if(Vector2.Distance((Vector2)transform.position, points[0]) < pointThreshold){
+                points.RemoveAt(0);
+
+                for (int i = 0; i < trailPath.positionCount - 2; i++)
+                {
+                    trailPath.SetPosition(i, trailPath.GetPosition(i + 1));
+                }
+                trailPath.positionCount--;
+            }
+
+        }
+    }
+    public float speed = 1f;
+    Rigidbody2D rigidbody;
     LineRenderer trailPath;
     public List<Vector2> points;
     Vector2 lastPosition;
+    Vector2 currentPosition;
     public float pointThreshold = 0.2f;
 
+    public AnimationCurve Landing;
+    float landingTime;
     //when we first click on the plane
     private void OnMouseDown()
     {
@@ -39,7 +87,7 @@ public class Plane : MonoBehaviour
     private void OnMouseDrag()
     {
         //get the current location of the mouse
-        Vector2 currentPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        currentPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
         //we check to see if the point is far enough away from the point
         if (Vector2.Distance(currentPosition, lastPosition) >= pointThreshold)
